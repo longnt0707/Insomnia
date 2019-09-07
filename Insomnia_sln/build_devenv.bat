16742f24a7d5f4ae04d7bbe533ce7da5d742e425
Nguyen The Long <longnt0707@gmail.com>
2019-09-07 23:03:22 +0900

    update script

--- Insomnia_sln/build_devenv.bat
+++ Insomnia_sln/build_devenv.bat
@@ -4,7 +4,8 @@ SET SlnName=""
 For %%i In (*.sln) Do SET SlnName=%%~ni
 echo %SlnName%.sln
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
